8a39a7604c6a45e4652f856778b4417b42b4be6e
ShawnPhang <palxiao@vip.qq.com>
2021-07-22 15:43:25 +0800

    feat: postcss兼容webpack

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,13 @@
-const prefix = import.meta.env
-// const prefix = process.env
+/*
+ * @Author: ShawnPhang
+ * @Date: 2021-07-12 15:03:31
+ * @Description: 
+ * @LastEditors: ShawnPhang
+ * @LastEditTime: 2021-07-22 15:37:01
+ * @site: book.palxp.com / blog.palxp.com
+ */
+// const prefix = import.meta.env
+const prefix = process.env
 
 export default {
     BASE_URL: prefix.BASE_URL,
